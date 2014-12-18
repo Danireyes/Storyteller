e15ebe18a03013ee0ed5d30d0e33e0980ff2bc08
Jorge Sanchez <jorjatorz@gmail.com>
2014-12-18 17:26:20 +0100

    xml ready

--- src/Game/FileManager.java
+++ src/Game/FileManager.java
@@ -63,7 +63,6 @@ public class FileManager {
 		displayersList = mXMLDocument.getDocumentElement().getElementsByTagName("displayer");
 		blocksList = mXMLDocument.getDocumentElement().getElementsByTagName("block");
 		
-		System.out.println(getDisplayerText("introduction"));
 		
 	 } catch (Exception e) {
 			e.printStackTrace();
@@ -82,10 +81,126 @@ public class FileManager {
 		{
 			Element node = (Element)displayersList.item(i);
 			
-			if(node.getAttribute("id") == id)
+			if(node.getAttribute("id").equals(id))
 			{
 				ret = node.getTextContent();
 			}
+		}
+		
+		return ret;
+	}
+	
+	public String getBlock(String id)
+	{
+		String ret = "";
+		for(int i = 0; i < blocksList.getLength(); i++)
+		{
+			Element node = (Element)blocksList.item(i);
+			
+			if(node.getAttribute("id").equals(id))
+			{	
+				NodeList childsList = node.getChildNodes();
+				
+				for(int j = 0; j < childsList.getLength(); j++)
+				{
+					Node nod = childsList.item(j);
+					
+					if(nod.getNodeName().equals("descriptionText"))
+					{
+						ret += nod.getTextContent();
+					}
+					
+					NodeList options;
+					
+					if(nod.getNodeName().equals("optionList"))
+					{
+						options = nod.getChildNodes();
+						
+						ret += "\n";
+						for(int h = 0; h < options.getLength(); h++)
+						{
+							if(options.item(h).getNodeName().equals("option"))
+							{
+								Element el = (Element)options.item(h);
+								ret += el.getAttribute("id") + ". " + options.item(h).getTextContent();
+								ret += "\n";
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return ret;
+	}
+	
+	String callOptionId(String blockId, String optionId) //Llama a la opcion y devuelve el proximo bloque (si no es un bloque printea la linked option) y devuelve su bloque
+	{
+		String ret = "";
+		
+		for(int i = 0; i < blocksList.getLength(); i++)
+		{
+			Element node = (Element)blocksList.item(i);
+			
+			if(node.getAttribute("id").equals(blockId))
+			{	
+				NodeList childsList = node.getChildNodes();
+				
+				for(int j = 0; j < childsList.getLength(); j++)
+				{
+					Node nod = childsList.item(j);					
+					NodeList options;
+					
+					if(nod.getNodeName().equals("optionList"))
+					{
+						options = nod.getChildNodes();
+						
+						for(int h = 0; h < options.getLength(); h++)
+						{
+							if(options.item(h).getNodeName().equals("option"))
+							{
+								Element el = (Element)options.item(h);
+								if(el.getAttribute("id").equals(optionId))
+								{
+									//Solo coge uno
+									ret = el.getAttribute("gotoBlock");
+									
+									if(!el.getAttribute("gotoLinkedResult").equals(""))
+									{
+										for(int k = 0; k < childsList.getLength(); k++)
+										{
+											Node nod2 = childsList.item(k);					
+											NodeList links;
+											
+											if(nod2.getNodeName().equals("linkedResultsList"))
+											{
+												links = nod2.getChildNodes();
+												
+												for(int l = 0; l < links.getLength(); l++)
+												{
+													if(links.item(l).getNodeName().equals("linkedResult"))
+													{
+														Element el1 = (Element)links.item(l);
+														
+														if(el.getAttribute("gotoLinkedResult").equals(el1.getAttribute("id")))
+														{
+															ret = el1.getAttribute("gotoBlock");
+															
+															System.out.println(el1.getTextContent());
+														}
+												}
+											}
+											
+										}
+									}
+									
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 		}
 		
 		return ret;
